95cbaaadc99a3b97a9dd04c995584c217c00881d
xLink <xLink@cybershade.org>
2016-01-16 00:57:14 +0000

    removed the unneeded side menu from category editor

--- Resources/views/backend/category/_layout.blade.php
+++ Resources/views/backend/category/_layout.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
-    <div class="col-md-{{ $col_one or '3'}}">
+    {{-- <div class="col-md-{{ $col_one or '3'}}">
         <div class="panel panel-default">
             <div class="panel-heading">
                 <div class="panel-title">Forum Category</div>
             </div>
             <div class="panel-body">@menu('backend_forum_category_menu')</div>
         </div>
-    </div>
+    </div> --}}
     <div class="col-md-{{ $col_two or '9'}}">
         @yield('category-form')
     </div>
